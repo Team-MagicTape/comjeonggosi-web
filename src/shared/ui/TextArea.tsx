4a6b0c44719ea6fb2bd1a89349e7b549526b9d21
choiyh0617 <6l0mrc00057@gmail.com>
2025-08-11 16:40:51 +0900

    feat : Add property interface

---
+++ src/shared/ui/TextArea.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import { TextareaHTMLAttributes } from 'react';
+
+const TextArea = ({ ...props }: TextareaHTMLAttributes<HTMLTextAreaElement>) => {
+  return (
+    <textarea
+      {...props}
+      className="mt-1 block w-full h-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+    />
+  );
+}
+
+export default TextArea

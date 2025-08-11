f92aede6db009a45b8446ad1fbf6f4c0d1c49ef2
choiyh0617 <6l0mrc00057@gmail.com>
2025-08-11 16:35:05 +0900

    feat : article write

---
+++ src/features/article/write/ui/ArticleForm.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import Input from "@/shared/ui/Input";
+import Button from "@/shared/ui/Button";
+import TextArea from "@/shared/ui/TextArea";
+import Select from "@/shared/ui/Select";
+import { useArticleWrite } from "../model/useArticleWrite";
+import { Category } from "@/entities/category/types/category";
+
+interface Props {
+  categories: Category[];
+}
+
+const WriteArticleForm = ({ categories }: Props) => {
+  const {
+    title,
+    setTitle,
+    content,
+    setContent,
+    handleSignup,
+    categoryId,
+    setCategoryId,
+  } = useArticleWrite();
+
+  return (
+    <form onSubmit={handleSignup} className="flex flex-col gap-2">
+      <Input
+        value={title}
+        placeholder="제목을 입력해주세요."
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <TextArea
+        value={content}
+        placeholder="내용을 입력해주세요."
+        onChange={(e) => setContent(e.target.value)}
+      />
+      <Select
+        value={categoryId}
+        onChange={(e) => setCategoryId(Number(e.target.value))}
+      >
+        <option value="">카테고리를 선택해주세요.</option>
+        {categories.map((category) => (
+          <option key={category.id} value={category.id}>
+            {category.name}
+          </option>
+        ))}
+      </Select>
+      <Button type="submit">완료</Button>
+    </form>
+  );
+};
+
+export default WriteArticleForm;

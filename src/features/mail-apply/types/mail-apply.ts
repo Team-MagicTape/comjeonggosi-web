f72b1ef5f1a9ab41990633f3bc73673613120057
leeyujin0527 <sksdldbwls0527@gmail.com>
2025-08-12 08:39:24 +0900

    feat : mail type

---
+++ src/features/mail-apply/types/mail-apply.ts
@@ -0,0 +1,14 @@
+export interface SubscribeMailRequest{
+    hour : number,
+    minute : number
+}
+
+export interface SubscribeMailResponse{
+    hour : number,
+    minute : number
+}
+
+export interface GetSubscriptionResponse {
+    hour: number;
+    minute: number;
+  }

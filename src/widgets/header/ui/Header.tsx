b84f4d51bc21c6e53339488fb7bba81779485e9f
choiyh0617 <6l0mrc00057@gmail.com>
2025-08-11 16:39:04 +0900

    feat : main article view

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -1,6 +1,7 @@
 import { fetchUser } from "@/entities/user/api/user";
 import UserAvatar from "@/entities/user/ui/UserAvatar";
 import Spacer from "@/shared/ui/Spacer";
+import Link from "next/link";
 
 const Header = async () => {
   const user = await fetchUser();
@@ -8,12 +9,14 @@ const Header = async () => {
   return (
     <header className="w-full fixed top-2 xl:top-6 z-10 flex justify-center px-2">
       <div className="w-full max-w-334 h-14 xl:h-20 bg-white border border-border rounded-2xl px-2 xl:px-8 flex items-center">
-        <h1 className="text-primary xl:text-3xl font-black">COMJEONGGOSI</h1>
+        <Link href="/">
+          <h1 className="text-primary xl:text-3xl font-black">COMJEONGGOSI</h1>
+        </Link>
         <Spacer />
         <UserAvatar size={56} user={user} />
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;

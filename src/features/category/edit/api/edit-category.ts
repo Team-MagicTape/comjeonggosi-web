f4d47ff3afa0f06aa71dbbc7cf385d4ced46b2a8
choiyh0617 <6l0mrc00057@gmail.com>
2025-08-12 15:02:06 +0900

    fix : axious -> apoiClient

--- src/features/category/edit/api/edit-category.ts
+++ src/features/category/edit/api/edit-category.ts
@@ -1,9 +1,9 @@
-import axios from "axios";
+import { apiClient } from "@/shared/libs/custom-axios";
 import { EditCategoryData } from "../types/edit-category-data";
 
 export const editCategory = async (id: string, data: EditCategoryData) => {
   try {
-    const res = await axios.put(`/admin/categories/${id}`, data);
+    const res = await apiClient.put(`/admin/categories/${id}`, data);
     return res;
   } catch (error) {
     console.error("editCategory error", error);

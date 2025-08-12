539a59e4d9d8af52a45eae4eaffbe1656b4a206a
choiyh0617 <6l0mrc00057@gmail.com>
2025-08-12 11:13:13 +0900

    fix : api url

--- src/features/article/delete/api/delete-article.ts
+++ src/features/article/delete/api/delete-article.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const deleteArticle = async (articleId: number) => {
   try {
-    const res = await axios.delete(`/admin/categories/${articleId}`);
+    const res = await axios.delete(`/admin/articles/${articleId}`);
     return res;
   } catch (error) {
     console.error("deleteArticle error", error);

65dada6576a940b1704da30c7b1d12c12816f90a
choiyh0617 <6l0mrc00057@gmail.com>
2025-08-12 13:58:47 +0900

    fix : write -> create

--- src/features/article/create/api/create-article.ts
+++ src/features/article/create/api/create-article.ts
@@ -6,7 +6,7 @@ export const createArticle = async (data: CreateArticleData) => {
     const res = await axios.post("/admin/articles", data);
     return res;
   } catch (error) {
-    console.error("writeArticle error", error);
+    console.error("createArticle error", error);
     throw error;
   }
 };

62f367762518716df6b1424750f0749195c493c6
choiyh0617 <6l0mrc00057@gmail.com>
2025-08-12 11:20:50 +0900

    fix : article detail

--- src/entities/article/api/article-detail.ts
+++ src/entities/article/api/article-detail.ts
@@ -1,9 +1,11 @@
 import { customFetch } from "@/shared/libs/custom-fetch";
 import { Article } from "../types/article";
 
-export const ArticleDetail = async (id: number): Promise<Article | undefined> => {
-   try {
-    const data = await customFetch.get<Article>(`/admin/articles/${id}`);
+export const ArticleDetail = async (
+  id: number
+): Promise<Article | undefined> => {
+  try {
+    const { data } = await customFetch.get<Article>(`/admin/articles/${id}`);
     return data;
   } catch (error) {
     console.error("fetchArticleDetail error", error);

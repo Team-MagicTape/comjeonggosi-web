3b857174ac4832dee6b4bca72e0bf22327d918ea
choiyh0617 <6l0mrc00057@gmail.com>
2025-08-14 10:58:34 +0900

    feat: article detail sidebar

--- src/app/(root)/articles/[id]/page.tsx
+++ src/app/(root)/articles/[id]/page.tsx
@@ -4,6 +4,7 @@ import { notFound } from "next/navigation";
 import DeleteButton from "@/features/delete-article/ui/DeleteButton";
 import EditButton from "@/features/edit-article/ui/EditButton";
 import { PathParams } from "@/shared/types/path-params";
+import ArticleSidebar from "@/entities/article/ui/ArticleSidebar";
 
 const ArticleDetailPage = async ({ params }: PathParams) => {
   const article = await getArticleDetail(Number(params));
@@ -13,17 +14,20 @@ const ArticleDetailPage = async ({ params }: PathParams) => {
   }
 
   return (
-    <div className="bg-white border border-border rounded-2xl p-4 xl:p-8 flex flex-col gap-6 items-start">
-      <span className="flex items-center justify-between w-full">
-        <h1 className="font-extrabold text-3xl">{article.title}</h1>
-        <div className="flex items-center gap-2">
-          <DeleteButton articleId={Number(params)} />
-          <EditButton articleId={Number(params)} />
-        </div>
-      </span>
-      <hr className="border border-gray-200 w-full" />
-      <p className="font-medium">{article.content}</p>
-    </div>
+    <div className="flex xl:flex-row gap-9 pr-[264px]">
+      <ArticleSidebar />
+      <div className="w-full bg-white border border-border rounded-2xl xl:p-8 flex flex-col gap-6">
+        <span className="flex items-center justify-between w-full">
+          <h1 className="font-extrabold text-3xl">{article.title}</h1>
+          {/* <div className="flex items-center gap-2">
+            <DeleteButton articleId={Number(params)} />
+            <EditButton articleId={Number(params)} />
+          </div> */}
+        </span>
+        <hr className="border border-gray-200 w-full" />
+        <p className="font-medium">{article.content}</p>
+      </div>
+      </div>
   );
 };
 

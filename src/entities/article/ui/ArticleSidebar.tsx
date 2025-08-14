82ec4df770a49c0a8570722671e5e589a8e4f6cb
choiyh0617 <6l0mrc00057@gmail.com>
2025-08-14 10:58:34 +0900

    feat: article detail sidebar

---
+++ src/entities/article/ui/ArticleSidebar.tsx
@@ -0,0 +1,10 @@
+const ArticleSidebar = () => {
+  return (
+    <div className="w-72 h-full bg-white border border-border rounded-2xl xl:p-6 flex flex-col gap-4">
+      <p className="font-bold text-xl">목차</p>
+      <hr className="border border-primary"/>
+    </div>
+  );
+};
+
+export default ArticleSidebar;

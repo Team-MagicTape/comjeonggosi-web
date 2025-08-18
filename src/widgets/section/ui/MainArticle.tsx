61472d63e3b03521dd1836dad6f7d693e3c20979
김태우 <taewoo080401@gmail.com>
2025-08-18 09:11:34 +0900

    fix: code crashes

--- src/widgets/section/ui/MainArticle.tsx
+++ src/widgets/section/ui/MainArticle.tsx
@@ -1,32 +1,38 @@
-import ArticleList from "@/entities/article/ui/ArticleList";
-import { fetchCategory } from "@/entities/category/api/fetch-category";
-import { fetchArticles } from "@/entities/article/api/fetch-articles";
+"use client";
+
+import Tabs from "@/widgets/tabs/ui/Tabs";
+import ArticleItem from "@/entities/article/ui/ArticleItem";
 import { Category } from "@/entities/category/types/category";
 import { Article } from "@/entities/article/types/article";
+import { useGetArticles } from "@/entities/article/model/useGetArticles";
+import { useArticleCategory } from "../model/useArticleCategory";
 
 interface Props {
-  Params?: { categoryId?: string };
   categories: Category[];
   initialData: Article[];
-  selectedCategoryId: number;
 }
 
-const MainArticle = async ({ Params }: Props) => {
-  const categories = await fetchCategory();
-  const selectedCategoryId = Params?.categoryId
-    ? parseInt(Params.categoryId)
-    : 1;
-
-  const articles = await fetchArticles(selectedCategoryId);
+const MainArticle = ({ categories, initialData }: Props) => {
+  const { category, categoryList, handleCategory } =
+    useArticleCategory(categories);
+  const { data, isLoading } = useGetArticles(
+    category?.value || "0",
+    initialData
+  );
 
   return (
     <div className="w-full xl:min-w-214 xl:w-auto bg-white border border-border rounded-2xl p-4 xl:p-8 flex flex-col gap-6 items-start">
       <h2 className="text-xl xl:text-2xl font-bold">아티클</h2>
-      <ArticleList
-        articles={articles || []}
-        categories={categories || []}
-        selectedCategoryId={selectedCategoryId}
+      <Tabs
+        tabs={categoryList}
+        selected={category}
+        setSelected={handleCategory}
       />
+      <div className="w-full grid grid-cols-1 xl:grid-cols-2 gap-4">
+        {isLoading || !Array.isArray(data) || data.length <= 0
+          ? Array.from({ length: 6 }).map((_, idx) => <ArticleItem key={idx} />)
+          : data.map((item) => <ArticleItem data={item} key={item.id} />)}
+      </div>
     </div>
   );
 };

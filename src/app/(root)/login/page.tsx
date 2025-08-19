da96de97e221b111b09fe541d1e67166de82e684
김태우 <taewoo080401@gmail.com>
2025-08-19 16:43:32 +0900

    debug: console

--- src/app/(root)/login/page.tsx
+++ src/app/(root)/login/page.tsx
@@ -3,7 +3,7 @@ import { cookies } from "next/headers"
 const Login = async () => {
   const cookie = await cookies();
 
-  console.log(cookie.toString());
+  console.log("cookie", cookie.toString());
 
   return (
     <div>Login</div>

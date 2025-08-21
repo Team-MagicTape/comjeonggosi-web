4ed4cee7bb188f88796287f2e67c0e350984cbce
leeyujin0527 <sksdldbwls0527@gmail.com>
2025-08-21 18:03:43 +0900

    Update page.tsx

--- src/app/(root)/mail/page.tsx
+++ src/app/(root)/mail/page.tsx
@@ -1,19 +1,15 @@
-import MailApplyForm from "@/features/mail-apply/ui/MailApplyFormClient";
+import MailApplyForm from "@/features/mail-apply/ui/MailApplyForm";
 import { getSubscription } from "@/features/mail-apply/api/get-subscription";
-import { getCategory } from "@/features/mail-apply/api/category";
-export interface SubscribeMail {
-  hour?: number;
-  minute?: number;
-}
+import { fetchCategories } from "@/entities/category/api/fetch-categories";
 
 const MailApply = async () => {
   const subscription = await getSubscription();
-  const categories = await getCategory();
+  const categories = await fetchCategories();
 
   return (
     <div>
       <MailApplyForm
-        initialHour={subscription.hour}
+        initialHour={subscription?.hour}
         categories={categories}
       />
     </div>

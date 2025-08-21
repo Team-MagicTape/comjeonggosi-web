807070eedd4d84746ceebf698ea5a27514c40e41
김태우 <taewoo080401@gmail.com>
2025-08-21 17:44:02 +0900

    fix: login button color

--- src/widgets/login-button/ui/LoginButton.tsx
+++ src/widgets/login-button/ui/LoginButton.tsx
@@ -4,7 +4,7 @@ import { login } from "@/widgets/login-modal/libs/modal-controller"
 
 const LoginButton = () => {
   return (
-    <button onClick={login.open} className="flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-gray-300 cursor-pointer">로그인</button>
+    <button onClick={login.open} className="flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-gray-600 bg-gray-200 cursor-pointer">로그인</button>
   )
 }
 

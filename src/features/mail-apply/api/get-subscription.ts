b6c8c1c41fdb8c80fb85f9da26217e249e5f5d6f
leeyujin0527 <sksdldbwls0527@gmail.com>
2025-08-21 18:02:21 +0900

    fix : api

--- src/features/mail-apply/api/get-subscription.ts
+++ src/features/mail-apply/api/get-subscription.ts
@@ -1,13 +1,12 @@
-import { SubscribeMail } from "../types/mail-apply";
+import { SubscribeMail } from "../types/get-mail";
 import { customFetch } from "@/shared/libs/custom-fetch";
-import { AxiosError } from "axios";
 
-export const getSubscription = async (): Promise<SubscribeMail> => {
+export const getSubscription = async () => {
   try{
     const { data } = await customFetch.get<SubscribeMail>("/questions/subscription");
     return data;
   }catch(error){
     console.error("구독 정보 가져오기 실패:", error);
-    return { hour: 0, categories: [] };
+    return null;
   }
 }; 

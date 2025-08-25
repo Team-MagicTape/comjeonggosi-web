ff6200b5bf89d7f1f4254fe1aa7b10a50576f0cf
김태우 <taewoo080401@gmail.com>
2025-08-25 16:21:15 +0900

    feat: logout

--- src/features/logout/ui/LogoutButton.tsx
+++ src/features/logout/ui/LogoutButton.tsx
@@ -1,10 +1,13 @@
 "use client";
 
 import Button from "@/shared/ui/Button";
+import { useLogout } from "../model/useLogout";
 
 const LogoutButton = () => {
+  const logout = useLogout();
+
   return (
-    <Button>로그아웃</Button>
+    <Button onClick={logout}>로그아웃</Button>
   )
 }
 

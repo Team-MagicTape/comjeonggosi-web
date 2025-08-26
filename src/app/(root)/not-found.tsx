f85a3f73081e7d6865cd7c18d6e1a3734b68893b
김태우 <taewoo080401@gmail.com>
2025-08-26 19:51:51 +0900

    fix: not found page

---
+++ src/app/(root)/not-found.tsx
@@ -0,0 +1,15 @@
+import CustomLink from "@/shared/ui/CustomLink"
+import { FileX } from "lucide-react"
+
+const NotFound = () => {
+  return (
+    <div className="w-full h-body flex flex-col gap-2">
+      <FileX className="text-red-500" size={44} />
+      <h1 className="text-3xl">404 NOT FOUND {":("}</h1>
+      <p>페이지또는 콘텐츠를 찾을 수 없습니다.</p>
+      <CustomLink href="/" className="text-primary">메인으로 돌아가기</CustomLink>
+    </div>
+  )
+}
+
+export default NotFound

57e69975c7ac7b83c0b42b25122c8fe09e38a109
김태우 <taewoo080401@gmail.com>
2025-08-26 20:02:23 +0900

    fix: not found full height

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@ import { FileX } from "lucide-react"
 
 const NotFound = () => {
   return (
-    <div className="w-full h-body flex flex-col items-center justify-center gap-2">
+    <div className="w-full h-screen flex flex-col items-center justify-center gap-2">
       <FileX className="text-red-500" size={56} />
       <h1 className="text-3xl">404 NOT FOUND {":("}</h1>
       <p>페이지를 찾을 수 없습니다.</p>

ff6eea5cb6871db49094f329caec7ffd647af299
김태우 <taewoo080401@gmail.com>
2025-08-26 16:22:29 +0900

    fix: type

--- src/widgets/section/model/useArticleCategoryStore.ts
+++ src/widgets/section/model/useArticleCategoryStore.ts
@@ -2,11 +2,11 @@ import { Tab } from "@/widgets/tabs/types/tab";
 import { create } from "zustand";
 
 interface State {
-  category: Tab | null;
-  setCategory: (category: Tab | null) => void;
+  category: Tab;
+  setCategory: (category: Tab) => void;
 }
 
 export const useArticleCategoryStore = create<State>((set) => ({
-  category: null,
+  category: { name: "디비", value: "1" },
   setCategory: (category) => set({ category }),
 }));

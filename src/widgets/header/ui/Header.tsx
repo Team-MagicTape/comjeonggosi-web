d43dad6d74124892f120ad652e9ebea3612208b4
김태우 <taewoo080401@gmail.com>
2025-08-26 20:12:15 +0900

    feat: logo

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -2,6 +2,7 @@ import { fetchUser } from "@/entities/user/api/fetch-user";
 import Spacer from "@/shared/ui/Spacer";
 import NavigationLink from "./NavigationLink";
 import LoginButton from "@/widgets/login-button/ui/LoginButton";
+import Image from "next/image";
 
 const Header = async () => {
   const user = await fetchUser();
@@ -9,9 +10,7 @@ const Header = async () => {
   return (
     <header className="w-full fixed top-7 xl:top-10 z-10 flex justify-center xl:px-13">
       <div className="w-full max-w-334 h-14 xl:h-20 bg-white border-b xl:border border-border xl:rounded-2xl px-2 xl:px-8 flex items-center gap-2">
-        <h1 className="text-primary text-xl xl:text-3xl font-black">
-          COMJEONGGOSI
-        </h1>
+        <Image src="/assets/logo.svg" alt="comjeonggosi" width={256} height={80} className="w-42 h-14 xl:w-64 xl:h-20" />
         <Spacer />
         <div className="hidden items-center gap-2 xl:flex">
           <NavigationLink href="/" name="홈" />

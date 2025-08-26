395850b31e7feb3220519cadf0de943ba869dd38
김태우 <taewoo080401@gmail.com>
2025-08-26 12:02:02 +0900

    fix: endpoint

--- src/entities/quiz/api/fetch-initial-quiz.ts
+++ src/entities/quiz/api/fetch-initial-quiz.ts
@@ -3,7 +3,7 @@ import { Quiz } from "../types/quiz";
 
 export const fetchInitialQuiz = async (categoryId: number) => {
   try{
-    const { data } = await customFetch.get<Quiz>(`/api/quizzes?categoryId=${categoryId}`);
+    const { data } = await customFetch.get<Quiz>(`/quizzes?categoryId=${categoryId}`);
     return data;
   }catch{
     return null;

5d47794157b9a7430f9592040af8a6cf11df280f
choiyh0617 <6l0mrc00057@gmail.com>
2025-08-26 20:22:25 +0900

    chore: Code improvements

--- src/app/(root)/admin/articles/[id]/edit/page.tsx
+++ src/app/(root)/admin/articles/[id]/edit/page.tsx
@@ -1,12 +1,17 @@
-import EditArticleForm from '@/features/edit-article/ui/EditArticleForm';
+import EditArticleForm from "@/features/edit-article/ui/EditArticleForm";
 import { PathParams } from "@/shared/types/path-params";
+import { getArticleDetail } from "@/entities/article/api/get-article-detail";
 
-const EditArticlePage = ({ params }: PathParams) => {
-  const articleId = Number(params);
+const EditArticlePage = async ({ params }: PathParams) => {
+  const article = await getArticleDetail(Number(params));
+
+  if (!article) {
+    return <div>게시글을 찾을 수 없습니다.</div>;
+  }
 
   return (
     <div>
-      <EditArticleForm articleId={articleId} />
+      <EditArticleForm articleId={Number(params)} article={article} />
     </div>
   );
 };

3731f42e6004cc36e223d2a8f60a50f298a7a9ec
김태우 <taewoo080401@gmail.com>
2025-08-26 20:00:04 +0900

    feat: content preparing, not found

--- src/app/(root)/not-found.tsx
+++ src/app/(root)/not-found.tsx
@@ -3,8 +3,8 @@ import { ToolCase } from "lucide-react"
 
 const NotFound = () => {
   return (
-    <div className="w-full h-body flex flex-col gap-2">
-      <ToolCase className="text-blue-500" size={44} />
+    <div className="w-full h-body flex flex-col items-center justify-center gap-2">
+      <ToolCase className="text-blue-500" size={56} />
       <h1 className="text-3xl">Preparing Now {":)"}</h1>
       <p>콘텐츠를 준비하는 중입니다!</p>
       <CustomLink href="/" className="text-primary">메인으로 돌아가기</CustomLink>
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,8 +3,8 @@ import { FileX } from "lucide-react"
 
 const NotFound = () => {
   return (
-    <div className="w-full h-body flex flex-col gap-2">
-      <FileX className="text-red-500" size={44} />
+    <div className="w-full h-body flex flex-col items-center justify-center gap-2">
+      <FileX className="text-red-500" size={56} />
       <h1 className="text-3xl">404 NOT FOUND {":("}</h1>
       <p>페이지를 찾을 수 없습니다.</p>
       <CustomLink href="/" className="text-primary">메인으로 돌아가기</CustomLink>

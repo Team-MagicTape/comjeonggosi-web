a2b67b24740dee44ee7a0c34ee6d13dcad1eec12
choiyh0617 <6l0mrc00057@gmail.com>
2025-08-26 20:34:12 +0900

    fix: params

--- src/app/(root)/admin/articles/[id]/edit/page.tsx
+++ src/app/(root)/admin/articles/[id]/edit/page.tsx
@@ -3,7 +3,9 @@ import { PathParams } from "@/shared/types/path-params";
 import { getArticleDetail } from "@/entities/article/api/get-article-detail";
 
 const EditArticlePage = async ({ params }: PathParams) => {
-  const article = await getArticleDetail(Number(params));
+  const { id } = await params;
+  const articleId = Number(id);
+  const article = await getArticleDetail(articleId);
 
   if (!article) {
     return <div>게시글을 찾을 수 없습니다.</div>;

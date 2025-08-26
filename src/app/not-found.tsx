693e807e62006a7a5431734ae6931888039b6e14
김태우 <taewoo080401@gmail.com>
2025-08-26 19:57:06 +0900

    feat: content preparing, not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,12 @@
 import CustomLink from "@/shared/ui/CustomLink"
-import { ToolCase } from "lucide-react"
+import { FileX } from "lucide-react"
 
 const NotFound = () => {
   return (
     <div className="w-full h-body flex flex-col gap-2">
-      <ToolCase className="text-blue-500" size={44} />
-      <h1 className="text-3xl">Preparing Now {":)"}</h1>
-      <p>콘텐츠를 준비하는 중입니다!</p>
+      <FileX className="text-red-500" size={44} />
+      <h1 className="text-3xl">404 NOT FOUND {":("}</h1>
+      <p>페이지를 찾을 수 없습니다.</p>
       <CustomLink href="/" className="text-primary">메인으로 돌아가기</CustomLink>
     </div>
   )

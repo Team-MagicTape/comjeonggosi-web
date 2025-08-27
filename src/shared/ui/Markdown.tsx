89dd54ac6d5cf0181d2f93bd1a84ef2a09147455
choiyh0617 <6l0mrc00057@gmail.com>
2025-08-27 17:00:24 +0900

    feat: Markdown responsive

--- src/shared/ui/Markdown.tsx
+++ src/shared/ui/Markdown.tsx
@@ -4,11 +4,33 @@ import ReactMarkdownPreview from "@uiw/react-markdown-preview";
 
 const Markdown = ({ content }: { content: string }) => {
   return (
-    <div data-color-mode="light">
-    <ReactMarkdownPreview
-      source={content}
-      className="wmde-markdown wmde-markdown-color"
-    />
+    <div
+      data-color-mode="light"
+      className="
+        wmde-markdown
+        wmde-markdown-color
+        [&_h1]:text-xl
+        [&_h2]:text-lg
+        [&_h3]:text-base
+        [&_h4]:text-sm
+        [&_p]:text-sm
+        [&_ul]:text-xs
+        [&_li]:text-xs
+        [&_ol]:text-xs
+        [&_hr]:!h-0.5
+        [&_hr]:!border-0
+
+        xl:[&_h1]:text-3xl
+        xl:[&_h2]:text-2xl
+        xl:[&_h3]:text-xl
+        xl:[&_h4]:text-lg
+        xl:[&_p]:text-base
+        xl:[&_ul]:text-base
+        xl:[&_li]:text-base
+        xl:[&_ol]:text-base
+      "
+    >
+      <ReactMarkdownPreview source={content} />
     </div>
   );
 };

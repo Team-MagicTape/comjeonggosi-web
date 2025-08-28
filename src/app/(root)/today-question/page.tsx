3e9764c8baa583eef22a6804eceea453290ac9bf
leeyujin0527 <sksdldbwls0527@gmail.com>
2025-08-28 19:53:38 +0900

    fix : today-question view

--- src/app/(root)/today-question/page.tsx
+++ src/app/(root)/today-question/page.tsx
@@ -1,15 +1,11 @@
 import CustomLink from "@/shared/ui/CustomLink";
 import { ToolCase } from "lucide-react";
+import TodayQuestionForm from "@/entities/today-question/ui/TodayQuestionForm";
 
 const TodayQuestion = () => {
   return (
-    <div className="w-full h-body flex flex-col items-center justify-center gap-2">
-      <ToolCase className="text-blue-500" size={56} />
-      <h1 className="text-3xl">Preparing Now {":)"}</h1>
-      <p>페이지를 준비하는 중입니다!</p>
-      <CustomLink href="/" className="text-primary">
-        메인으로 돌아가기
-      </CustomLink>
+    <div>
+      <TodayQuestionForm/>
     </div>
   );
 };

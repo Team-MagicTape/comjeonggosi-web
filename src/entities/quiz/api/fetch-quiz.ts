c589330536cc046bd67d6704056b7c5a3ff8a881
김태우 <taewoo080401@gmail.com>
2025-09-08 20:06:24 +0900

    fix: api client to bare axios in quiz fetching

--- src/entities/quiz/api/fetch-quiz.ts
+++ src/entities/quiz/api/fetch-quiz.ts
@@ -1,9 +1,9 @@
+import axios from "axios";
 import { Quiz } from "../types/quiz";
-import { apiClient } from "@/shared/libs/custom-axios";
 
 export const fetchQuiz = async (categoryId: string, hidden?: string) => {
   try {
-    const { data } = await apiClient.get<Quiz>(`/api/quizzes?categoryId=${categoryId}${hidden ? `&hidden=${hidden}` : ""}`);
+    const { data } = await axios.get<Quiz>(`/api/quizzes?categoryId=${categoryId}${hidden ? `&hidden=${hidden}` : ""}`);
     console.log("quiz: ", data);
     return data;
   } catch {

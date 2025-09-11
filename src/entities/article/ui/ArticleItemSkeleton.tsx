71e48e5d492b15cefb06b668425606e91f0ab558
leeyujin0527 <sksdldbwls0527@gmail.com>
2025-09-11 17:21:48 +0900

    fix : ui type

--- src/entities/article/ui/ArticleItemSkeleton.tsx
+++ src/entities/article/ui/ArticleItemSkeleton.tsx
@@ -5,8 +5,8 @@ import Skeleton from "@/shared/ui/Skeleton";
 const ArticleItemSkeleton = () => {
   return (
     <div className="w-full p-5 rounded-item bg-bg border border-border space-y-2">
-      <Skeleton width="50%" height={28} radius={6} />
-      <Skeleton width="70%" height={20} radius={4} />
+      <Skeleton width="50%" height="28px" radius={6} />
+      <Skeleton width="70%" height="20px" radius={4} />
     </div>
   );
 };
--- src/entities/quiz/ui/MySubmissionItemSkeleton.tsx
+++ src/entities/quiz/ui/MySubmissionItemSkeleton.tsx
@@ -5,24 +5,24 @@ const MySubmissionItemSkeleton = () => {
     <div className="relative p-6 rounded-item border border-border">
       <div className="flex items-center gap-3 mb-1">
         <div className="flex items-center gap-2">
-          <Skeleton width="60%" height={20} radius={6} />
-          <Skeleton width={80} height={24} radius={12} />
+          <Skeleton width="60%" height="20px" radius={6} />
+          <Skeleton width="80px" height="24px" radius={12} />
         </div>
       </div>
 
       <div className="space-y-3 mb-4">
         <div className="flex items-center gap-2">
-          <Skeleton width={50} height={18} radius={4} /> {/* "내 답안:" 라벨 */}
+          <Skeleton width="50px" height="18px" radius={4} />
           <div className="flex-1 flex gap-4 flex-wrap">
-            <Skeleton width="40%" height={20} radius={6} />
-            <Skeleton width="30%" height={20} radius={6} />
+            <Skeleton width="40%" height="20px" radius={6} />
+            <Skeleton width="30%" height="20px" radius={6} />
           </div>
         </div>
       </div>
 
       <div className="w-full flex items-center gap-1 text-lightgray text-sm">
-        <Skeleton width={80} height={16} radius={4} />
-        <Skeleton width={10} height={16} radius={4} />
+        <Skeleton width="80px" height="16px" radius={4} />
+        <Skeleton width="10px" height="16px" radius={4} />
       </div>
     </div>
   );

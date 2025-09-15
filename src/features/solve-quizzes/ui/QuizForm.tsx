a89f57197c9ef2e856aba0ef6e1179075e611feb
choiyh0617 <6l0mrc00057@gmail.com>
2025-09-15 22:05:24 +0900

    fix: edit text

--- src/features/solve-quizzes/ui/QuizForm.tsx
+++ src/features/solve-quizzes/ui/QuizForm.tsx
@@ -171,9 +171,8 @@ const QuizForm = ({ categories, initialQuiz, user }: Props) => {
                       handleSettingChange={handleSettingChange}
                     />
                   </div>
-                  <div className="flex">
-                    <HelpText text="TIP1. 1,2,3,4 입력하여 답을 선택할 수 있습니다." />
-                    <HelpText text="TIP2. 스페이스바 누르면 다음문제로 넘어갑니다." />
+                  <div className="flex items-center justify-center">
+                    <HelpText text="TIP1. 1,2,3,4 입력하여 답을 선택할 수 있고 스페이스바를 누르면 다음 문제로 넘어갑니다." />
                   </div>
                 </div>
               ))

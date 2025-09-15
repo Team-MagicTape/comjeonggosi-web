5043489f95165aab6681d12e7f8eacaa042378d0
김태우 <taewoo080401@gmail.com>
2025-09-15 12:22:07 +0900

    feat: miss typing

--- src/entities/quiz/api/fetch-initial-submissions.ts
+++ src/entities/quiz/api/fetch-initial-submissions.ts
@@ -3,7 +3,7 @@ import { MySubmission } from "../types/my-submission";
 
 export const fetchInitialSubmissions = async () => {
   try{
-    const { data } = await customFetch.get<MySubmission[]>("/quizzses/submissions/my");
+    const { data } = await customFetch.get<MySubmission[]>("/quizzes/submissions/my");
     return data;
   }catch{
     return [];

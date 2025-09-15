10b0a3eac425093a8992ed5a2c719523487b90fd
choiyh0617 <6l0mrc00057@gmail.com>
2025-09-16 08:29:05 +0900

    feat: add margin

--- src/features/solve-quizzes/ui/QuizForm.tsx
+++ src/features/solve-quizzes/ui/QuizForm.tsx
@@ -170,7 +170,7 @@ const QuizForm = ({ categories, initialQuiz, user }: Props) => {
                       settings={settings}
                       handleSettingChange={handleSettingChange}
                     />
-                    <div className="flex items-center justify-center">
+                    <div className="flex items-center justify-center mb-3">
                       <HelpText text="TIP1. 1,2,3,4 입력하여 답을 선택할 수 있고 스페이스바를 누르면 다음 문제로 넘어갑니다." />
                     </div>
                   </div>

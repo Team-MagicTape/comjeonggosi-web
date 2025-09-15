1982046bd1034b5bb375d87614367cbe1aad6a24
choiyh0617 <6l0mrc00057@gmail.com>
2025-09-15 21:51:58 +0900

    fix: Edit location

--- src/features/solve-quizzes/ui/WorkbookQuizForm.tsx
+++ src/features/solve-quizzes/ui/WorkbookQuizForm.tsx
@@ -114,6 +114,10 @@ const WorkbookQuizForm = ({ data }: Props) => {
                             />
                           );
                         })}
+                        <div className="flex">
+                          <HelpText text="TIP1. 1,2,3,4 입력하여 답을 선택할 수 있습니다." />
+                          <HelpText text="TIP2. 스페이스바 누르면 다음문제로 넘어갑니다." />
+                        </div>
                       </div>
                     )}
 
@@ -138,10 +142,6 @@ const WorkbookQuizForm = ({ data }: Props) => {
                       settings={settings}
                       handleSettingChange={handleSettingChange}
                     />
-                  </div>
-                  <div className="flex">
-                    <HelpText text="TIP1. 1,2,3,4 입력하여 답을 선택할 수 있습니다." />
-                    <HelpText text="TIP2. 스페이스바 누르면 다음문제로 넘어갑니다." />
                   </div>
                 </div>
               ))

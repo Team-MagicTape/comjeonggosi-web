70abfad2d3d7673934c72a436c08cfd12b91dc07
choiyh0617 <6l0mrc00057@gmail.com>
2025-10-16 17:51:03 +0900

    fix: review

--- src/features/edit-notice/api/fetch-edit-notices.ts
+++ src/features/edit-notice/api/fetch-edit-notices.ts
@@ -3,7 +3,7 @@ import { NoticesType } from "@/entities/notices/types/notices";
 
 export const FetchEditNotices = async (id: string, editData: NoticesType) => {
   try {
-    const { data } = await apiClient.put(`/notices/${id}`, editData);
+    const { data } = await apiClient.put(`/api/notices/${id}`, editData);
     return data;
   } catch (error) {
     console.error("공지사항 수정 오류", error);

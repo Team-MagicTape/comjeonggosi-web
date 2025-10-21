baee88c88021fed80abf5df2cb5288c8611bea79
choiyh0617 <6l0mrc00057@gmail.com>
2025-10-21 20:21:23 +0900

    fix: error

--- src/entities/article/api/get-article-detail.ts
+++ src/entities/article/api/get-article-detail.ts
@@ -2,7 +2,7 @@ import { customFetch } from "@/shared/libs/custom-fetch";
 import { Article } from "../types/article";
 
 export const getArticleDetail = async (
-  id: number = 1
+  id: string
 ): Promise<Article | undefined> => {
   try {
     const { data } = await customFetch.get<Article>(`/articles/${id}`);
